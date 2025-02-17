ed7830fd3a105aafa210271d1d0263f8b475004e
111linblink <146273461+111linblink@users.noreply.github.com>
2025-02-17 14:50:47 -0600

    Mapa del sitio - NavBar - TerminosPrivacidad:))

--- frontend/src/app/components/inicio/inicio.component.ts
+++ frontend/src/app/components/inicio/inicio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
---
+++ frontend/src/app/components/mapa-sitio/mapa-sitio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mapa-sitio',
+  standalone: true,
+  imports: [],
+  templateUrl: './mapa-sitio.component.html',
+  styleUrl: './mapa-sitio.component.css'
+})
+export class MapaSitioComponent {
+
+}
